114ba027eb37ccea99269b27f6f8fe1aef199dcd
mustafizrahman055-hub <mustafizrahman055@gmail.com>
2026-09-03 08:10:06 +0600

    Solve Question 2

--- answer.js
+++ answer.js
@@ -4,3 +4,22 @@ function describeValue(val) {
   const truthiness = val ? "truthy" : "falsy";
   return `${type} | ${truthiness}`;
 }
+
+// Question No. 2: Bangladesh Weekend Machine
+function getDayType(day) {
+  const formattedDay = day.toLowerCase();
+  
+  switch (formattedDay) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+    default:
+      return "Invalid Day";
+  }
+}
